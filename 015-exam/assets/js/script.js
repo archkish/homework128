be9cd50cfa3ee0ea4187f43b1bae6b6d99f0625d
Artur Radchenko <kisharchi@gmail.com>
2023-12-22 23:31:05 +0100

    add exam work

---
+++ 015-exam/assets/js/script.js
@@ -0,0 +1,18 @@
+
+// const header = document.querySelector('.header');
+// const first = document.querySelector('.hero');
+// const headerHeight = header.offsetHeight;
+// const firstHeight = first.offsetHeight;
+// let lastScrollTop = 0;
+
+// window.addEventListener('scroll', () => {
+// 	let scrollDistance = window.scrollY;
+
+// 	if (scrollDistance >= firstHeight + headerHeight) {
+// 		header.classList.add('header--fixed');
+// 		first.style.marginTop = `${headerHeight}px`;
+// 	} else {
+// 		header.classList.remove('header--fixed');
+// 		first.style.marginTop = null;
+// 	}
+// })
